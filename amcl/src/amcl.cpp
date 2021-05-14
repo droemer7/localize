48053f9caacc98ecaf75d71adc07ad138f6b8792
droemer7 <droemer7@gmail.com>
2021-05-13 21:36:42 -0700

    Temporary change to print count

--- amcl/src/amcl.cpp
+++ amcl/src/amcl.cpp
@@ -211,6 +211,7 @@ void AMCL::sampleUpdate()
     }
     // Update distribution with new sample set
     dist_.update(samples_, s);
+    printStats();
   }
 }
 
@@ -247,7 +248,7 @@ void AMCL::printStats(const std::string& header) const
 {
   printf("%s", header.c_str());
   printf("Sample count = %lu\n", dist_.count());
-  printf("Weight average [curr] = %.2e\n", dist_.weightAvgCurr());
-  printf("Weight average [fast] = %.2e\n", dist_.weightAvgFast());
-  printf("Weight average ratio = %.2e\n", dist_.weightAvgRatio());
+  // printf("Weight average [curr] = %.2e\n", dist_.weightAvgCurr());
+  // printf("Weight average [fast] = %.2e\n", dist_.weightAvgFast());
+  // printf("Weight average ratio = %.2e\n", dist_.weightAvgRatio());
 }
